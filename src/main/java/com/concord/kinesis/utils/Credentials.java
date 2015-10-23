31323a0d8790295750e2a478ab50ec173ac96063
Cole Brown <bigswim@gmail.com>
2015-10-23 13:20:23 -0400

    Add precondition checks to ctor

--- src/main/java/com/concord/kinesis/utils/Credentials.java
+++ src/main/java/com/concord/kinesis/utils/Credentials.java
@@ -1,12 +1,15 @@
 package com.concord.kinesis.utils;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.google.common.base.Preconditions;
 
 public final class Credentials implements AWSCredentials {
   private final String key;
   private final String secret;
 
   public Credentials(String key, String secret) {
+    Preconditions.checkNotNull(key);
+    Preconditions.checkNotNull(secret);
     this.key = key;
     this.secret = secret;
   }
